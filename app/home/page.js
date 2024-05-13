b57de8201615baedcdea2758e0260b1476ada60f
B.sairamireddy <88982057+sairamireddy6@users.noreply.github.com>
2024-05-13 15:32:10 +0530

    Add files via upload

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+"use client"
+import Image from "next/image";
+import TopicList from "@/component/topicList";
+
+export default function Home() {
+  return (
+    <>
+    <TopicList/>
+    </>
+  );
+}
